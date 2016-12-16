42e938531921264e149d0fc16206575e26683152
Eelco Bosklopper <dhrstoff@gmail.com>
2016-12-16 11:54:43 +0100

    Full page spinner

--- src/public/js/general.js
+++ src/public/js/general.js
@@ -2,12 +2,16 @@ var ViewModel = function() {
     var self  = this;
 
     self.greeting = ko.observable("hello!");
-    
+
     self.buttonDisabled = ko.observable(false);
 
     self.disableButton = function() {
+        $('<span id="loader" class="loader loadervisible"></span>').appendTo('body');
+
         self.buttonDisabled(true);
+
         setTimeout(function () {
+            $('#loader').remove();
             self.buttonDisabled(false);
         }, 3000);
     };
